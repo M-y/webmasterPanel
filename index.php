862654487518fca984a6718762e3cd13b4f00886
Muhammed YILDIRIM <ben@muhammed.im>
2013-08-19 02:36:53 +0300

    Sürüm 0.0.1
    İskeleti oluşturuyorum.

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+require_once('kutuphane/ayarlar.php');
 
+require(anaKlasor . 'kutuphane/webmasterPanel.php');
+$webmasterPanel = new webmasterPanel(); /// @warning bu nesne tanımlanmadan önce echo kesinlikle kullanılmamalı
 
+/// Bu sayfanın başlığı (En basitinden <title></title> için kullanılacak)
+define('baslik', 'Webmaster Panel');
+
+$moduller = $webmasterPanel -> moduller();
+var_dump($moduller);
+
+$webmasterPanel -> htmlCiktisiVer();
 ?>
