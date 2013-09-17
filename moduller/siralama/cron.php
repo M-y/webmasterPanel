59ff6405597bccbfc5e1609f8472552d565618d4
Muhammed YILDIRIM <ben@muhammed.im>
2013-09-17 17:08:43 +0300

    Sürüm 0.9.0
    Sıralama ve uptime modülünde geliştirmeler.
    Link kontrol modülü eklendi.

--- moduller/siralama/cron.php
+++ moduller/siralama/cron.php
@@ -23,6 +23,22 @@ if ( $siteler ) {
       $oku[date('d-m-Y')] = googleIndex($site);
       $webmasterPanel -> ayarKaydet("siralama_googleIndex_{$site}", $oku);
     }
+    
+    /// Alexa Rank
+    $oku = $webmasterPanel -> ayarOku("siralama_alexaRank_{$site}");
+    if ( !isset($oku[date('d-m-Y')]) ) {
+      $oku[date('d-m-Y')] = alexaRank($site);
+      $webmasterPanel -> ayarKaydet("siralama_alexaRank_{$site}", $oku);
+    }
+    
+    /// SERP
+    $kelimeler = $webmasterPanel -> ayarOku("siralama_SERP_{$site}");
+    if ( is_array($kelimeler) )
+      for ( $kelimeSayac = 0; $kelimeSayac < count($kelimeler); $kelimeSayac++ )
+	if ( !isset($kelimeler[$kelimeSayac]['istatistik'][date('d-m-Y')]) ) {
+	  $kelimeler[$kelimeSayac]['istatistik'][date('d-m-Y')] = googleSiralama($kelimeler[$kelimeSayac]['kelime'], $site);
+	  $webmasterPanel -> ayarKaydet("siralama_SERP_{$site}", $kelimeler);
+	}
   }
 }
 ?>
--- moduller/siralama/siralama.php
+++ moduller/siralama/siralama.php
@@ -11,7 +11,7 @@ require(anaKlasor . '/moduller/siralama/fonksiyonlar.php');
 
 echo '<div class="sutun_50">';
 echo '<h2>Site Sıralamaları Modülü</h2>';
-  $webmasterPanel -> mesaj('Bu modül sitelerinizin pagerank, alexa, belli kelimelerde google sıralaması gibi bilgileri belirli zaman aralıklarında kaydederek grafiksel olarak size sunar. ');
+  $webmasterPanel -> mesaj('Bu modül sitelerinizin pagerank, alexa, belli kelimelerde google sıralaması gibi bilgileri günlük olarak kaydederek grafiksel olarak size sunar. ');
 echo '</div>';
 echo '<div class="clearfix"></div>';
 
@@ -20,6 +20,12 @@ if ( isset($_GET['PR']) )
 else
 if ( isset($_GET['googleIndex']) )
   require('googleIndex.php');
+else
+if ( isset($_GET['alexaRank']) )
+  require('alexaRank.php');
+else
+if ( isset($_GET['SERP']) )
+  require('SERP.php');
 else
   require('anasayfa.php');
 
